bf7cb5d170e434f50cc36d79d6136b8ef0cb1e41
CJ Keeney <cliffordkeeney@gmail.com>
2022-05-07 19:22:06 -0700

    refactor into using react context

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import theme from "./theme";
 import "./App.scss";
 // noinspection ES6UnusedImports slice used for a commented out debug line
 import {prop, slice} from 'ramda';
+import AchievementProvider from "./components/achievement-ctx";
 
 const FILTER_MAP = {
   All: () => true,
@@ -22,8 +23,6 @@ const FILTER_NAMES = Object.keys(FILTER_MAP);
 function App(props) {
   // const [achievements, setAchievements] = useState(slice(0, 10, props.achievements)); // use this line for debugging fewer (10) achievements.
   const [achievements, setAchievements] = useState(props.achievements);
-  const [completed, setCompleted] = useState({});
-  const [backlogged, setBacklogged] = useState({});
   const [filter, setFilter] = useState("All");
 
   function addAchievement(name) {
@@ -36,31 +35,6 @@ function App(props) {
     setAchievements([...achievements, newAchievement]);
   }
 
-  function toggleBacklogged(id) {
-    setBacklogged({
-      /**
-       * a lot going on here:
-       * ...backlogged is the "spread" operator.  We're using it here because we are already passing an object to setBacklogged.
-       * If we did not use the spread operator for backlogged, the new backlogged object would look like:
-       * {backlogged: oldBackloggedValue, id: true/false} (all the old backlogged data nested under a backlogged key in the object).
-       *
-       * putting square brackets around 'id' in the object key tells the compiler that id is a variable and that
-       *  it should use the value of id as the key, not the string 'id'.
-       *
-       *  ! negates the value of backlogged[id]
-       */
-      ...backlogged,
-      [id]: !backlogged[id],
-    })
-  }
-
-  function toggleCompleted(id) {
-    setCompleted({
-      ...completed,
-      [id]: !completed[id],
-    })
-  }
-
   const list = achievements
     .filter(FILTER_MAP[filter])
     .map((achievement) => (
@@ -69,13 +43,13 @@ function App(props) {
         img={achievement.icon}
         name={achievement.name}
         description={achievement.description}
-        completed={prop(achievement.id, completed)}
-        backlogged={prop(achievement.id, backlogged)}
+        // completed={prop(achievement.id, completed)}
+        // backlogged={prop(achievement.id, backlogged)}
         key={achievement.id}
         // the individual list items are only ever going to pass in their own ids.  instead of giving them a general function to pass in id,
         // we can give them a function they call without passing in anything.  probably doesnt matter much except it keeps the mostly-UI components dumber/less logic code.
-        toggleCompleted={() => toggleCompleted(achievement.id)}
-        toggleBacklogged={() => toggleBacklogged(achievement.id)}
+        // toggleCompleted={() => toggleCompleted(achievement.id)}
+        // toggleBacklogged={() => toggleBacklogged(achievement.id)}
       />
     ));
 
@@ -93,13 +67,16 @@ function App(props) {
 
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Form addAchievement={addAchievement} />
-        <h2>{headingText}</h2>
-        <Box>{filterList}</Box>
-        <ul>{list}</ul>
-      </div>
+      <AchievementProvider>
+        <div className="App">
+          <Form use={addAchievement} />
+          <h2>{headingText}</h2>
+          <Box>{filterList}</Box>
+          <ul>{list}</ul>
+        </div>
+      </AchievementProvider>
     </ThemeProvider>
+
   );
 }
 
