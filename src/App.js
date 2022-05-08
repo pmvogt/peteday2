8db6f235948d13026a9ed68a24d4670ca961345a
peter vogt <peter@petervogt.co>
2022-05-08 01:29:18 -0500

    merge cj changes and resolve conflicts

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@
 import React, { useState } from "react";
 import { Box, ThemeProvider } from "theme-ui";
 import { nanoid } from "nanoid";
-import { FixedSizeList as List } from "react-window";
-
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import ListItem from "./components/ListItem";
 
 import theme from "./theme";
 import "./App.scss";
+// noinspection ES6UnusedImports slice used for a commented out debug line
+import { prop, slice } from "ramda";
+import AchievementProvider from "./components/achievement-ctx";
 
 const FILTER_MAP = {
   All: () => true,
@@ -20,6 +21,7 @@ const FILTER_MAP = {
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
+  // const [achievements, setAchievements] = useState(slice(0, 10, props.achievements)); // use this line for debugging fewer (10) achievements.
   const [achievements, setAchievements] = useState(props.achievements);
   const [filter, setFilter] = useState("All");
 
@@ -33,33 +35,6 @@ function App(props) {
     setAchievements([...achievements, newAchievement]);
   }
 
-  function toggleBacklogged(id) {
-    const backloggedAchievements = achievements.map((achievement) => {
-      // if this task has the same ID as the edited task
-      if (id === achievement.id) {
-        // use object spread to make a new object
-        // whose `completed` prop has been inverted
-        return { ...achievement, backlogged: !achievement.backlogged };
-      }
-      return achievement;
-    });
-    setAchievements(backloggedAchievements);
-    console.log(backloggedAchievements);
-  }
-
-  function toggleCompleted(id) {
-    const completedAchievements = achievements.map((achievement) => {
-      // if this task has the same ID as the edited task
-      if (id === achievement.id) {
-        // use object spread to make a new object
-        // whose `completed` prop has been inverted
-        return { ...achievement, completed: !achievement.completed };
-      }
-      return achievement;
-    });
-    setAchievements(completedAchievements);
-  }
-
   const list = achievements
     .filter(FILTER_MAP[filter])
     .map((achievement) => (
@@ -68,11 +43,7 @@ function App(props) {
         img={achievement.icon}
         name={achievement.name}
         description={achievement.description}
-        completed={achievement.completed}
-        backlogged={achievement.backlogged}
         key={achievement.id}
-        toggleCompleted={toggleCompleted}
-        toggleBacklogged={toggleBacklogged}
       />
     ));
 
@@ -88,18 +59,16 @@ function App(props) {
   const achievementNoun = list.length !== 1 ? "achievements" : "achivements";
   const headingText = `${list.length} ${achievementNoun} remaining`;
 
-  const Row = ({ index, style }) => <div style={style}>{list[index]}</div>;
-
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Form addAchievement={addAchievement} />
-        <h2>{headingText}</h2>
-        <Box>{filterList}</Box>
-        <List height={800} itemCount={1200} itemSize={35} width={600}>
-          {Row}
-        </List>
-      </div>
+      <AchievementProvider>
+        <div className="App">
+          <Form use={addAchievement} />
+          <h2>{headingText}</h2>
+          <Box>{filterList}</Box>
+          <ul>{list}</ul>
+        </div>
+      </AchievementProvider>
     </ThemeProvider>
   );
 }
